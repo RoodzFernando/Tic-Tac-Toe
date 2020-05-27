f40fb6f3bd77dac79388bd5b416d014888e503bb
Marvellous Ubani <marvellousubani@gmail.com>
2020-05-27 21:38:34 +0100

    encapsulating global code in function objects

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const notification = document.querySelector('.notification');
 const restartBtn = document.querySelector('.restart-btn');
 
 const Gameboard = {
-  table: [],
+  table: Array.from(Array(9).keys()),
   winComs: [
     '012',
     '345',
@@ -17,7 +17,16 @@ const Gameboard = {
 
 };
 const Player = (name, marker) => {
-  const changeCurrentPlayer = () => (currentPlayer === players[0] ? currentPlayer = players[1] : currentPlayer = players[0]);
+  const player1 = Player(prompt('Player One\'s name: '), 'X');
+  const player2 = Player(prompt('Player Two\'s name: '), 'O');
+  const players = [];
+  players.push(player1, player2);
+  let currentPlayer = players[0];
+  console.log(currentPlayer)
+  
+  const changeCurrentPlayer = () => (
+    currentPlayer === players[0] ? 
+    currentPlayer = players[1] : currentPlayer = players[0]);
 
   const playerMove = function (id) {
     Gameboard.table.splice(id, 1, this.marker);
@@ -52,64 +61,65 @@ const Player = (name, marker) => {
     name, marker, checkWin, playerMove, draw, changeCurrentPlayer,
   };
 };
-Gameboard.table = Array.from(Array(9).keys());
 
-const player1 = Player(prompt('Player One\'s name: '), 'X');
-const player2 = Player(prompt('Player Two\'s name: '), 'O');
-const players = [];
+(function game(){
 
-players.push(player1, player2);
-let currentPlayer = players[0];
+  Player();
 
-function gameFinish() {
-  gameboard.removeEventListener('click', gameClick);
-  restartBtn.style.display = 'block';
-}
-
-const gameStart = () => {
-  Gameboard.table = Array.from(Array(9).keys());
-  gameboard.innerHTML = '';
-  notification.innerHTML = '';
-  restartBtn.style.display = 'none';
-  render();
-  gameboard.addEventListener('click', gameClick);
-};
-
-function gameClick(e) {
-  if (e.target.textContent == ' ') {
-    currentPlayer.playerMove(e.target.id);
-    render();
-  }
-
-  if (currentPlayer.checkWin()) {
-    notification.textContent = `${currentPlayer.name} wins`;
-    gameFinish();
-  }
-
-  if (currentPlayer.draw) {
-    currentPlayer.draw();
+  function gameFinish() {
+    gameboard.removeEventListener('click', gameClick);
     restartBtn.style.display = 'block';
   }
 
-  currentPlayer.changeCurrentPlayer();
-}
+  const gameStart = () => {
+    Gameboard.table = Array.from(Array(9).keys());
+    gameboard.innerHTML = '';
+    notification.innerHTML = '';
+    restartBtn.style.display = 'none';
+    render();
+    gameboard.addEventListener('click', gameClick);
+  };
 
-gameboard.addEventListener('click', gameClick);
-restartBtn.addEventListener('click', gameStart);
-
-
-function render() {
-  for (let i = 0; i < Gameboard.table.length; i++) {
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('id', i);
-    gameboard.appendChild(newDiv);
-    if (typeof Gameboard.table[i] === 'number') {
-      newDiv.textContent = ' ';
-    } else {
-      newDiv.textContent = Gameboard.table[i];
+  function gameClick(e) {
+    if (e.target.textContent == ' ') {
+      currentPlayer.playerMove(e.target.id);
+      render();
     }
+
+    if (currentPlayer.checkWin()) {
+      notification.textContent = `${currentPlayer.name} wins`;
+      gameFinish();
+    }
+
+    if (currentPlayer.draw) {
+      currentPlayer.draw();
+      restartBtn.style.display = 'block';
+    }
+
+    currentPlayer.changeCurrentPlayer();
   }
-}
+
+  gameboard.addEventListener('click', gameClick);
+  restartBtn.addEventListener('click', gameStart);
 
 
-render();
+  function render() {
+    for (let i = 0; i < Gameboard.table.length; i++) {
+      const newDiv = document.createElement('div');
+      newDiv.setAttribute('id', i);
+      gameboard.appendChild(newDiv);
+      if (typeof Gameboard.table[i] === 'number') {
+        newDiv.textContent = ' ';
+      } else {
+        newDiv.textContent = Gameboard.table[i];
+      }
+    }
+
+    render();
+  }
+
+
+})()
+
+
+
