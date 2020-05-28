71d6e2aa63637142309beb0713eb48eeaf0de436
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-05-28 13:40:02 -0400

    Fixed linter errors

--- scripts/main.js
+++ scripts/main.js
@@ -3,108 +3,101 @@ const notification = document.querySelector(".notification");
 const restartBtn = document.querySelector(".restart-btn");
 
 const Gameboard = {
-  table: Array.from(Array(9).keys()),
-  winComs: ["012", "345", "678", "036", "147", "258", "048", "246"],
+    table: Array.from(Array(9).keys()),
+    winComs: ["012", "345", "678", "036", "147", "258", "048", "246"],
 };
 
 const Player = (name, marker) => ({
-  name,
-  marker,
+    name,
+    marker,
 });
 
 
 function gameClick(e) {
-  game.changeCurrentPlayer();
+    game.changeCurrentPlayer();
 
-  if (e.target.textContent == " ") {
-    game.playerMove(e.target.id);
-    render();
-  }
+    if (e.target.textContent === " ") {
+        game.playerMove(e.target.id);
+        render();
+    }
 
-  if (game.checkWin()) {
-    notification.textContent = `${currentPlayer.name} wins`;
-    game.gameFinish();
-  } else if (game.draw()) {
-    game.draw();
-  }
+    if (game.checkWin()) {
+        notification.textContent = `${currentPlayer.name} wins`;
+        game.gameFinish();
+    } else if (game.draw()) {
+        game.draw();
+    }
 }
 
 function render() {
-  for (let i = 0; i < Gameboard.table.length; i++) {
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("id", i);
-    gameboard.appendChild(newDiv);
-    if (typeof Gameboard.table[i] === "number") {
-      newDiv.textContent = " ";
-    } else {
-      newDiv.textContent = Gameboard.table[i];
+    for (let i = 0; i < Gameboard.table.length; i += 1) {
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("id", i);
+        gameboard.appendChild(newDiv);
+        if (typeof Gameboard.table[i] === "number") {
+            newDiv.textContent = " ";
+        } else {
+            newDiv.textContent = Gameboard.table[i];
+        }
     }
-  }
 }
 
-
-
-
 const game = {
-  gameStart: () => {
-    Gameboard.table = Array.from(Array(9).keys());
-    gameboard.innerHTML = "";
-    notification.innerHTML = "";
-    restartBtn.style.display = "none";
-    render();
-    gameboard.addEventListener("click", gameClick);
-  },
-  changeCurrentPlayer: () => {
-    if (currentPlayer === players[0]) {
-      currentPlayer = players[1];
-    } else {
-      currentPlayer = players[0];
-    }
-  },
+    gameStart: () => {
+        Gameboard.table = Array.from(Array(9).keys());
+        gameboard.innerHTML = "";
+        notification.innerHTML = "";
+        restartBtn.style.display = "none";
+        render();
+        gameboard.addEventListener("click", gameClick);
+    },
+    changeCurrentPlayer: () => {
+        if (currentPlayer === players[0]) {
+            currentPlayer = players[1];
+        } else {
+            currentPlayer = players[0];
+        }
+    },
 
-  checkWin() {
-    let result = false;
-    let xs = "";
-    for (let i = 0; i < Gameboard.table.length; i++) {
-      if (Gameboard.table[i] == currentPlayer.marker) {
-        xs += i.toString();
-      }
-    }
+    checkWin() {
+        let result = false;
+        let xs = "";
+        for (let i = 0; i < Gameboard.table.length; i += 1) {
+            if (Gameboard.table[i] === currentPlayer.marker) {
+                xs += i.toString();
+            }
+        }
 
-    Gameboard.winComs.forEach((word) => {
-      if (
-        xs.includes(word[0]) &&
-        xs.includes(word[1]) &&
-        xs.includes(word[2])
-      ) {
-        result = true;
-      }
-    });
-    return result;
-  },
+        Gameboard.winComs.forEach((word) => {
+            if (
+                xs.includes(word[0]) &&
+                xs.includes(word[1]) &&
+                xs.includes(word[2])
+            ) {
+                result = true;
+            }
+        });
+        return result;
+    },
 
-  draw() {
-    if (
-      !this.checkWin() &&
-      Gameboard.table.every((elem) => typeof elem === "string")
-    ) {
-      notification.textContent = "It's a draw!";
-      restartBtn.style.display = "block";
-    }
-  },
+    draw() {
+        if (!this.checkWin() &&
+            Gameboard.table.every((elem) => typeof elem === "string")
+        ) {
+            notification.textContent = "It's a draw!";
+            restartBtn.style.display = "block";
+        }
+    },
 
-  gameFinish: () => {
-    gameboard.removeEventListener("click", gameClick);
-    restartBtn.style.display = "block";
-  },
-  playerMove(id) {
-    Gameboard.table.splice(id, 1, currentPlayer.marker);
-    gameboard.innerHTML = "";
-  },
+    gameFinish: () => {
+        gameboard.removeEventListener("click", gameClick);
+        restartBtn.style.display = "block";
+    },
+    playerMove(id) {
+        Gameboard.table.splice(id, 1, currentPlayer.marker);
+        gameboard.innerHTML = "";
+    },
 };
-
-
-
 
 game.gameStart();
 gameboard.addEventListener("click", gameClick);
