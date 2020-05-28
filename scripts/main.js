6263230c8a33b1a287bdb81f737619f7f2f0a7a8
Marvellous Ubani <marvellousubani@gmail.com>
2020-05-28 17:30:21 +0100

    using prettier to adjust formatting issues on code

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,10 @@
-const gameboard = document.querySelector('#gameboard');
-const notification = document.querySelector('.notification');
-const restartBtn = document.querySelector('.restart-btn');
-
+const gameboard = document.querySelector("#gameboard");
+const notification = document.querySelector(".notification");
+const restartBtn = document.querySelector(".restart-btn");
 
 const Gameboard = {
   table: Array.from(Array(9).keys()),
-  winComs: [
-    '012',
-    '345',
-    '678',
-    '036',
-    '147',
-    '258',
-    '048',
-    '246',
-  ],
-
+  winComs: ["012", "345", "678", "036", "147", "258", "048", "246"],
 };
 
 const Player = (name, marker) => ({
@@ -26,11 +15,11 @@ const Player = (name, marker) => ({
 const game = {
   gameStart: () => {
     Gameboard.table = Array.from(Array(9).keys());
-    gameboard.innerHTML = '';
-    notification.innerHTML = '';
-    restartBtn.style.display = 'none';
+    gameboard.innerHTML = "";
+    notification.innerHTML = "";
+    restartBtn.style.display = "none";
     render();
-    gameboard.addEventListener('click', gameClick);
+    gameboard.addEventListener("click", gameClick);
   },
   changeCurrentPlayer: () => {
     if (currentPlayer === players[0]) {
@@ -40,10 +29,9 @@ const game = {
     }
   },
 
-
   checkWin() {
     let result = false;
-    let xs = '';
+    let xs = "";
     for (let i = 0; i < Gameboard.table.length; i++) {
       if (Gameboard.table[i] == currentPlayer.marker) {
         xs += i.toString();
@@ -51,35 +39,41 @@ const game = {
     }
 
     Gameboard.winComs.forEach((word) => {
-      if (xs.includes(word[0]) && xs.includes(word[1]) && xs.includes(word[2])) {
+      if (
+        xs.includes(word[0]) &&
+        xs.includes(word[1]) &&
+        xs.includes(word[2])
+      ) {
         result = true;
       }
     });
     return result;
   },
 
-
   draw() {
-    if (!this.checkWin() && Gameboard.table.every((elem) => typeof elem === 'string')) {
-      notification.textContent = 'It\'s a draw!';
-      restartBtn.style.display = 'block';
+    if (
+      !this.checkWin() &&
+      Gameboard.table.every((elem) => typeof elem === "string")
+    ) {
+      notification.textContent = "It's a draw!";
+      restartBtn.style.display = "block";
     }
   },
 
   gameFinish: () => {
-    gameboard.removeEventListener('click', gameClick);
-    restartBtn.style.display = 'block';
+    gameboard.removeEventListener("click", gameClick);
+    restartBtn.style.display = "block";
   },
   playerMove(id) {
     Gameboard.table.splice(id, 1, currentPlayer.marker);
-    gameboard.innerHTML = '';
+    gameboard.innerHTML = "";
   },
 };
 
 function gameClick(e) {
   game.changeCurrentPlayer();
 
-  if (e.target.textContent == ' ') {
+  if (e.target.textContent == " ") {
     game.playerMove(e.target.id);
     render();
   }
@@ -94,23 +88,22 @@ function gameClick(e) {
 
 function render() {
   for (let i = 0; i < Gameboard.table.length; i++) {
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('id', i);
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", i);
     gameboard.appendChild(newDiv);
-    if (typeof Gameboard.table[i] === 'number') {
-      newDiv.textContent = ' ';
+    if (typeof Gameboard.table[i] === "number") {
+      newDiv.textContent = " ";
     } else {
       newDiv.textContent = Gameboard.table[i];
     }
   }
 }
 
-
 game.gameStart();
-gameboard.addEventListener('click', gameClick);
-restartBtn.addEventListener('click', game.gameStart);
+gameboard.addEventListener("click", gameClick);
+restartBtn.addEventListener("click", game.gameStart);
 
-const player1 = Player(prompt('Player 1 name:'), 'X');
-const player2 = Player(prompt('Player 2 name:'), 'O');
+const player1 = Player(prompt("Player 1 name:"), "X");
+const player2 = Player(prompt("Player 2 name:"), "O");
 const players = [player1, player2];
 let currentPlayer = players[0];
